d49da78d95e5d464f15df74523701c4e413ad800
dakatapetrov <daka7a@gmail.com>
2014-04-26 23:48:24 +0300

    Can now get categories for given type

--- controllers/type_controller.rb
+++ controllers/type_controller.rb
@@ -4,5 +4,19 @@ module ArtversityServer
       status 200
       body Type.all_data.to_json
     end
+
+    get '/:id', provides: :json do
+      status 200
+      type = Type.find(id: params[:id])
+
+      if type.nil?
+        status 404
+      else
+        status 200
+
+        type.categories_data.to_json
+      end
+
+    end
   end
 end
