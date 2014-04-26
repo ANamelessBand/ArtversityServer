19cd77dcbd91efb0aaa6c62d85591c4c7ca4f665
Hristo Vladev <hr.vladev@gmail.com>
2014-04-26 18:29:26 +0300

    Add medias association to performance

--- models/performance.rb
+++ models/performance.rb
@@ -1,6 +1,7 @@
 class Performance < Sequel::Model
   many_to_one :type
   many_to_many :categories
+  one_to_many :medias
 
   def validate
     validates_presence [:last_seen, :times_tagged, :location_latitude, :location_longitude]
