5a71ab5417c716cb9a344cac22417e6e22eb764f
dakatapetrov <daka7a@gmail.com>
2014-04-26 12:16:11 +0300

    Remove obsolete migration. Update Gemfile.lock

--- database/migrations/005_create_performances_categories.rb
+++
@@ -1,9 +0,0 @@
-Sequel.migration do
-  change do
-    create_table(:medias) do
-      primary_key :id, index: true
-      foreign_key :performance_id, :performances, null: false
-      foreign_key :category_id, :categories, null: false
-    end
-  end
-end
