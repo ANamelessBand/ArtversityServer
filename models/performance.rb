56b4f1d9e93ff16627bedb606ac27b23164aef91
Hristo Vladev <hr.vladev@gmail.com>
2014-04-26 18:31:14 +0300

    Add performance methods returning data in a hash

--- models/performance.rb
+++ models/performance.rb
@@ -6,4 +6,32 @@ class Performance < Sequel::Model
   def validate
     validates_presence [:last_seen, :times_tagged, :location_latitude, :location_longitude]
   end
+
+  def full_data
+    core_data.merge media_data
+  end
+
+  def media_data
+    # Think about this represation
+    {
+      pictures: [],
+      videos: [],
+      audios: []
+    }
+  end
+
+  def core_data
+    {
+      id: id,
+      rating: 42,
+      location: {
+        latitude: location_latitude,
+        longitude: location_longitude
+      },
+      times_tagged: times_tagged,
+      type: type.name,
+      categories: categories.map(&:name),
+      picture: 'pictures.last.url'
+    }
+  end
 end
