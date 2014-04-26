c8e675deffb4c038678ba5508e62291a8a5f437a
dakatapetrov <daka7a@gmail.com>
2014-04-26 15:08:39 +0300

    Change Date to DateTime in performances model and migration

--- database/migrations/003_create_performances.rb
+++ database/migrations/003_create_performances.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   change do
     create_table(:performances) do
       primary_key :id, index: true
-      Date :last_date, null: false
+      DateTime :last_seen, null: false
       Integer :times_tagged, null: false
       Float :location_latitude, null: false
       Float :location_longitude, null: false
--- models/performance.rb
+++ models/performance.rb
@@ -3,6 +3,6 @@ class Performance < Sequel::Model
   many_to_many :categories
 
   def validate
-    validates_presence [:last_date, :times_tagged, :location_latitude, :location_longitude]
+    validates_presence [:last_seen, :times_tagged, :location_latitude, :location_longitude]
   end
 end
