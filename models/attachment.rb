c2fd80f4ae38b02f73ffd568b1b15e471c5f4f5b
Hristo Vladev <hr.vladev@gmail.com>
2014-04-27 12:59:38 +0300

    Create ImageUploader and mount it to Attachment

--- models/attachment.rb
+++ models/attachment.rb
@@ -1,5 +1,21 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process resize_to_fit: [nil, 200]
+  end
+end
+
 class Attachment < Sequel::Model
   many_to_one :performance
+
+  mount_uploader :filename, ImageUploader
 
   def validate
     validates_presence [:filename, :type]
